31c83665dc5180b72299af4f7579bb423868634b
Fredy Cerron <fredy.cerron.contractor@bbva.com>
2023-09-13 09:17:22 -0500

    feat:PERAR24655 - PERAR24849: inputs added

--- artifact/transactions/RBVDT401-01-PE/src/main/java/com/bbva/rbvd/AbstractRBVDT40101PETransaction.java
+++ artifact/transactions/RBVDT401-01-PE/src/main/java/com/bbva/rbvd/AbstractRBVDT40101PETransaction.java
@@ -1,6 +1,8 @@
 package com.bbva.rbvd;
 
 import com.bbva.elara.transaction.AbstractTransaction;
+import com.bbva.rbvd.dto.lifeinsrc.simulation.ParticipantDTO;
+import java.util.List;
 
 /**
  * In this class, the input and output data is defined automatically through the setters and getters.
@@ -10,4 +12,11 @@ public abstract class AbstractRBVDT40101PETransaction extends AbstractTransactio
 	public AbstractRBVDT40101PETransaction(){
 	}
 
+
+	/**
+	 * Return value for input parameter participants
+	 */
+	protected List<ParticipantDTO> getParticipants(){
+		return (List<ParticipantDTO>)this.getParameter("participants");
+	}
 }
